5fa30a24220520eb751fa236d4af74e9f89f0a6e
RoUchiha <swaggsikh@gmail.com>
2022-02-19 00:58:56 -0600

    commit tree 86541af0438560eeb008fe6ab7cd2d05cb8eb6c8

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+let cards = []
+function writeCards(nameArray, event) {
+    for (let i = 0; i < nameArray.length; i++) {
+        cards.push(`Thank you, ${nameArray[i]}, for the wonderful ${event} gift!`);
+    }
+    return cards
+}
+
+
+function countDown(int) {
+    let i = int 
+    while (i > -1) {
+        console.log(i);
+        i--;
+    }
+
+}
